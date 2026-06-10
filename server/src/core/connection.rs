8dfb66b1c8524e5602cb193920f40b2878a9e8d6
Javier FILEIV <javier.fileiv@gmail.com>
2026-06-10 10:56:45 +0200

    feat: start broker dev
    - connection module removed.
    - basic functionality and tests for some broker messages like Connect
      and Broadcast.

--- server/src/core/connection.rs
+++ server/src/core/connection.rs
@@ -1,14 +1,15 @@
 use crate::auth::client;
-use crate::core::broker::{ BrokerClient, BrokerEvent, BrokerToClientMsg };
-use futures_util::{ Stream, StreamExt, Sink, SinkExt };
-use tokio::sync::mpsc::{ self, UnboundedSender };
+use crate::core::broker::{BrokerClient, BrokerEvent, BrokerToClientMsg};
+use futures_util::{Sink, Stream, StreamExt};
 use std::net::SocketAddr;
-use tokio_tungstenite::tungstenite::{ Error, Message };
+use tokio::sync::mpsc::{self, UnboundedSender};
+use tokio_tungstenite::tungstenite::{Error, Message};
 
 pub async fn handle<S>(ws: S, addr: SocketAddr, broker_sender: UnboundedSender<BrokerEvent>)
-    where S: Stream<Item = Result<Message, Error>> + Sink<Message>
+where
+    S: Stream<Item = Result<Message, Error>> + Sink<Message>,
 {
-    let (mut sink, mut source) = ws.split();
+    let (_, mut source) = ws.split();
 
     // Get first frame where client authentification data should be.
     let Some(Ok(Message::Text(_data))) = source.next().await else {
@@ -28,18 +29,21 @@ pub async fn handle<S>(ws: S, addr: SocketAddr, broker_sender: UnboundedSender<B
     let broker_client = BrokerClient {
         user: username,
         addr,
-        room: room,
+        room,
         broker_to_client: from_broker,
     };
 
-    let msg = BrokerEvent::Connect { client: broker_client };
+    let msg = BrokerEvent::Connect {
+        client: broker_client,
+    };
 
     broker_sender.send(msg).unwrap();
 
     while let Some(Ok(msg)) = source.next().await {
         if let Message::Text(_) = msg {
-            // on_msg_broadcast(addr, msg);
+            todo!("Handle broadcast message")
         }
     }
     eprintln!("{addr} disconnected");
+    todo!("Spawn thread to handle received messages from broker.");
 }
