2717bef851208b9b4f91c3d3a5eed7cdeaf07201
Javier FILEIV <javier.fileiv@gmail.com>
2026-06-11 18:14:11 +0200

    feat(server): allow server to start on specified port

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,22 @@
 mod auth;
 mod core;
 
+use clap::Parser;
 use core::server;
 use tokio::net::TcpListener;
 
+#[derive(Parser, Debug)]
+#[command(name = "chatter-server", about = "WebSocket chat server")]
+struct Args {
+    #[arg(short, long, default_value = "8080")]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let args = Args::parse();
+    let addr = format!("127.0.0.1:{}", args.port);
+    let listener = TcpListener::bind(&addr).await?;
 
     server::run(listener).await?;
     Ok(())
